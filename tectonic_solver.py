e05146d3fa34728349aebc435653710b4019cd82
ZK38UJ <Jaap.van.Diermen@ing.com>
2024-12-31 15:05:23 +0100

    new boards

--- tectonic_solver.py
+++ tectonic_solver.py
@@ -314,7 +314,8 @@ if __name__ == "__main__":
         ["9x5 - level 7 moeilijk (100) (outside neighbourhood)", "t5.board.9x5.csv", "t5.layout.9x5.csv"],
         ["9x11- level 7 heel moeilijk (11)", "board.9x11.11.csv", "layout.9x11.11.csv"],
         ["4x5 - level 8 (46)","board.4x5.46.l8.csv", "layout.4x5.46.l8.csv"],
-
+        ["4x5 - level 9 (93)", "board.4x5.93.l9.csv", "layout.4x5.93.l9.csv"],
+        ["9x5 - level 8 (72)", "board.9x5.72.l8.csv", "layout.9x5.72.l8.csv"],
     ]
 
     #
@@ -325,7 +326,7 @@ if __name__ == "__main__":
     #
     # index=int(input("your choice:"))
 
-    index=7
+    index=9
 
     # file_path = '/Users/ZK38UJ/PycharmProjects/tectonic-solver/tst/t4.board.9x11.csv'
     # layout_path = '/Users/ZK38UJ/PycharmProjects/tectonic-solver/tst/t4.layout.9x11.csv'
